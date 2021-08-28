8b32cf21685a8524ffb8f9b507a672fcd86309e8
iamsubha <subhajeetdey07@gmail.com>
2021-08-29 02:35:38 +0530

    pay

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -52,7 +52,7 @@ const OrderScreen = ({ match, history }) => {
 
   useEffect(() => {
     const addPayPalScript = async () => {
-      // deysubha
+      // deysubha paypal
       const { data: clientId } = await axios.get('/api/config/paypal')
       const script = document.createElement('script')
       script.type = 'text/javascript'
