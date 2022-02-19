0fd6ea5dab74ede0397d8f09bb26dac5b0735f76
iamsubha <subhajeetdey07@gmail.com>
2022-02-19 20:55:47 +0530

    changes

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const orderSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
       require: true,
-      ref: "User",
+      ref: 'User',
     },
     orderItems: [
       {
@@ -16,7 +16,7 @@ const orderSchema = mongoose.Schema(
         product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
-          ref: "Product",
+          ref: 'Product',
         },
       },
     ],
@@ -71,7 +71,7 @@ const orderSchema = mongoose.Schema(
   {
     timestamps: true,
   }
-);
+)
 
-const Order = mongoose.model("Order", orderSchema);
-export default Order;
+const Order = mongoose.model('Order', orderSchema)
+export default Order
