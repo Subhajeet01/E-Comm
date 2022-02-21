eb22059f776b46dc3503db0d6d3f89c1c4374209
iamsubha <subhajeetdey07@gmail.com>
2022-02-21 15:54:27 +0530

    pagination cont

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -5,14 +5,16 @@ import Product from '../components/Product'
 import { listProducts } from '../actions/productActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import Paginate from '../components/Paginate'
 
 const Homescreen = ({ match }) => {
   const keyword = match.params.keyword || ''
   const pageNumber = match.params.pageNumber || 1
 
   const dispatch = useDispatch()
+  // details of the products as well as total pages and current page number
   const productList = useSelector((state) => state.productList)
-  const { loading, products, error } = productList
+  const { loading, products, error, pages, page } = productList
 
   useEffect(() => {
     dispatch(listProducts(keyword, pageNumber))
@@ -26,13 +28,20 @@ const Homescreen = ({ match }) => {
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
+        </>
       )}
     </>
   )
